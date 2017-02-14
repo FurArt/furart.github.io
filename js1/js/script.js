2ae16ba493791ea46cb3f8b3dc97459c170179be
unknown <iiionerart@gmail.com>
2017-02-14 16:59:44 +0200

    Add null and 0

--- js1/js/script.js
+++ js1/js/script.js
@@ -1,36 +1,39 @@
-var number = prompt('Ведите число:','');
-var exponent = Math.round(prompt('Ведите его сепень (целое число):',''));
+var number = 0;
+console.log(typeof(number));
+number = +prompt('Ведите число:', '' );
+console.log('number'+typeof(number));
+var exponent = Math.round(prompt('Ведите его сепень (целое число):', ''));
   function pow(number, exponent) {
-    if (exponent === 0) { // for 0
-      var result = 1;
-      return result;
+      if ( (exponent === 0))  { // for 0
+        var result = 1;
     } else if (exponent > 0 ){ // for exponent more than 0
-      var result = number;
-      for (var i = 1; i < exponent; i++) {
+        var result = number;
+        for (var i = 1; i < exponent; i++) {
         result *= number;
-      }
-      return result;
+        } return result;
     } else { // for exponent before 0
-      var result = 1 / number;
-      for (var i = -1; i > exponent; i--) {
+        var result = 1 / number;
+        for (var i = -1; i > exponent; i--) {
         result /=  number;
-      }
-      return result;
+        } return result;
     }
   }
   pow(number, exponent);
+  console.log(pow(number, exponent));
+  console.log('exponent'+typeof(exponent));
 if ( ( number && exponent ) == null) {
   alert ('Укажите числа, не оставляйте пустые поля');
-} else if (isNaN(pow(number, exponent))) {
-  alert ('Укажите числа, буквы в степени никчему');
-} else if (isNaN(number)) {
-  alert ('Укажите числа, буквы в степени никчему');
-} else if (isNaN(exponent)) {
-  alert ('Укажите числа, буквы в степени никчему');
+} else if ( number === 0 ) {
+  alert ('Укажите число, какое вы хотите добить степень');
+} else if ( isNaN( pow(number, exponent) ) ) {
+  alert ('Укажите числа');
+} else if ( isNaN(number) ) {
+  alert ('Укажите числа, буквы в степени никчему 2');
+} else if ( isNaN(exponent) ) {
+  alert ('Укажите числа, буквы в степени никчему 3');
 } else {
   alert ('Ваше число: '+pow(number, exponent));
 }
-
 
 
 
--- js1/js/script_2.js
+++ js1/js/script_2.js
@@ -2,7 +2,8 @@ var nameBase = [];
 for (var i = 0; i < 5; i++) {
   nameBase.push(prompt('Ведите имя',''));
 }
-var customerName = prompt('Ведите имя пользывателя','');
+var customerName = 0
+customerName = prompt('Ведите имя пользывателя','');
 var curentName = '';
 function nameChесk() {
   for (var i = 0; i < nameBase.length; i++) {
@@ -13,8 +14,14 @@ function nameChесk() {
   }
 }
 nameChесk();
-if (curentName.length>0){
+if ((nameBase[0] || nameBase[1] || nameBase[2] || nameBase[3] || nameBase[4]) == null) {
+  alert( 'Нажатием отмена Вы ничего не решите.' )
+}  else if (curentName.length>0){
   alert(curentName+',вы успешно вошли');
+} else if ( customerName === 0 ) {
+  alert( 'Укажите хоть какие даные' )
+} else if ( (nameBase[0].length || nameBase[1].length || nameBase[2].length || nameBase[3].length || nameBase[4].length) <= 0) {
+  alert( 'Укажите хоть какие даные' )
 } else {
   alert('Имя пользователя не существует');
 }

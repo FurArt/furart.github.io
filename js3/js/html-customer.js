2e889fbe22a0985c869a5f7e2c785c23502ebdda
FurArt <iiionerart@gmail.com>
2017-02-21 14:43:10 +0200

    js3

--- js3/js/html-customer.js
+++ js3/js/html-customer.js
@@ -1,16 +1,50 @@
-var mSecond = 1;
-function timer() {
-  for (var i = 0; i > 0; i++) {
-    mSecond++;
-  }
-  console.log(mSecond);
+var ms = 1;
+var a = 1;
+var s = 1;
+var m = 1;
+function time(){
+  var someTime = new Date();
+  ms = someTime.getTime();
+  a = ms % 1000;
+  var b = ms / 1000;
+  s = Math.floor(b % 60);
+  var c = b / 60;
+  m = Math.floor(c % 60);
 };
+// time();
+document
 
-var programTimer = setInterval(timer, 1);
-console.log(programTimer);
+setTimeout(function () {
 
-function displayTime(){
-  var a = mSecond % 10;
-  console.log(a);
-};
-displayTime();
+}, 10);
+console.log(ms)
+console.log(a)
+console.log(s)
+console.log(m)
+
+
+// html
+
+var head =   {
+    bootstrap: function (){
+      document.createElement('link');
+      this.href = 'css/bootstrap.css';
+      this.rel = 'stylesheet';
+    },
+    bootstrapTheme: function creatTagElement() {
+      var tagElement = document.createElement('link');
+      tagElement.setAttribute ( 'href', 'css/bootstrap-theme.css' );
+      tagElement.setAttribute ( 'rel', 'stylesheet' );
+      return tagElement;
+    },
+    customer: function creatTagElement() {
+      var tagElement = document.createElement('link');
+      tagElement.setAttribute ( 'href', 'css/customer.css' );
+      tagElement.setAttribute ( 'rel', 'stylesheet' );
+      return tagElement;
+    },
+  };
+
+  var headHtml = document.getElementsByTagName('head');
+  console.log(headHtml);
+  headHtml.appendChild(header.bootstrapTheme());

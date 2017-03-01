2985267b26dd6c52a662d09dad8191f75e2dc07d
FurArt <iiionerart@gmail.com>
2017-03-01 23:28:21 +0200

    js4 done

--- js4/js/customer.js
+++ js4/js/customer.js
@@ -1,7 +1,7 @@
   $ (function () {
     var $findControl = $('.form-control');
     var $findButton = $('#show');
-    $findButton.click( function () {
+    $findButton.click( function () { // функция на клик
         if ($(".form-help").hasClass('hidden')) {
           var $find= $('.form-help').fadeTo(500, 1, function () {
             $(this).removeClass('hidden')})
@@ -10,14 +10,8 @@
             $(this).addClass('hidden')
           })
         }
-
-
-        console.log($find);
       });
-
-
-
-    $findControl.mouseenter( function() {
+    $findControl.mouseenter( function() { // функция наведение мыши
       var $find = $(this).siblings('.form-help').fadeTo(500, 1, function () {
         $(this).removeClass('hidden')
       })
@@ -27,9 +21,16 @@
         $(this).addClass('hidden')
       })
     });
-    console.log($findButton);
-
-
-    // .siblings('.form-help')
+    var $linkFindClass =  $(".link-content");
+    $linkFindClass.click(function () {
+      if ($(this).hasClass("active")) {//проверка скликивание по 1 кнопке
+      } else {
+        $(".active").removeClass('active');
+        var $href = $(this).attr('href');
+        var $findId = $($href);
+        $findId.addClass('active')
+        $(this).addClass('active');
+      };
+    });
 
 });

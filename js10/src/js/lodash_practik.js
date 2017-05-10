4ae385a4de25264d3d311a5fe2805cd065974347
FurArt <iiionerart@gmail.com>
2017-05-10 22:31:31 +0300

    js10 done

--- js10/src/js/lodash_practik.js
+++ js10/src/js/lodash_practik.js
@@ -21,9 +21,13 @@ if (data.get.status === 200) {
     } catch (e) {
       console.log(e);
     } finally {
-      data.skills = [];
-      data.name = [];
-      data.friends= [];
+      data.skills=[];
+      data.name=[];
+      data.friends=[];
+      data.friendsName=[];
+      data.friendsNameAll=[];
+      data.friendsQualityName=[];
+
 
     }
 
@@ -37,14 +41,25 @@ try {
     function lodash() {
       _.forEach(data.base, function(v,k){
         data.skills.push(v.skills);
-        data.friends.push(v.friends.length);
+        data.friends.push({'friends':v.friends.length});
+        data.friendsName.push(v.friends);
       });
       _.filter(data.base, function(v) {
-      data.name.push(v.name)
+      data.name.push({'name':v.name})
+      });
+      _.forEach(_.flattenDeep(data.friendsName), function(v,k) {
+        data.friendsNameAll.push(v.name);
       });
 
+      data.friendsShort = _.uniq(_.sortBy(data.friendsNameAll));
       data.skillsShort = _.uniq(_.sortBy(_.flattenDeep(data.skills)));
-      data.friendsQuality = _.sortBy(_.zipObject(data.name, data.friends));
+      data.friendsQuality = _.sortBy(_.concat(_.zip(data.name, data.friends)), [function (o) {
+        return  o[1].friends;
+      }]);
+       _.forEach(_.flattenDeep(data.friendsQuality), function (v,k) {
+      data.friendsQualityName.push( v.name );
+      });
+      //
       // data.friendsSort = _.sortBy(data.friendsQuality);
       // _.sortBy(data.skills, function(a,b,c) {
       //    console.log(a);
@@ -54,6 +69,9 @@ try {
       // });
   };
   lodash();
+  $(function () {
+    $('body').html('<p> skills:'+data.skillsShort+'</p><p> This people have friends'+data.friendsQualityName+'</p><p> friends name'+data.friendsShort+'</p>');
+  });
   }
 }, 1000);
 

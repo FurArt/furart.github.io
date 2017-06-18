81903bb5a64642267daebdf5010e710201d348c9
FurArt <iiionerart@gmail.com>
2017-06-18 20:05:34 +0300

    add js change masonoru with

--- final/src/js/controller.js
+++ final/src/js/controller.js
@@ -5,53 +5,54 @@ try {
     function( ) {
       return {
         Controller : function (model, view) {
+
           var that = this;
+          try {
+            function getRandomArbitrary(min, max) {
+              return Math.floor(Math.random() * (max - min)) + min;
+            }; //random
 
-          function getRandomArbitrary(min, max) {
-            return Math.floor(Math.random() * (max - min)) + min;
-          }; //random
-
-          view.element.inputBtn.addEventListener('click', sendRequest);
-          view.element.inputIdeas.addEventListener('keypress', function(e) {
-            if (e.keyCode===13) {
-              sendRequest();
-            };
-          });
+            view.element.inputBtn.addEventListener('click', sendRequest);
+            view.element.inputIdeas.addEventListener('keypress', function(e) {
+              if (e.keyCode===13) {
+                sendRequest();
+              };
+            });
 
 
-          function sender(r) {
-            var xhr = new XMLHttpRequest();                                                        //&image_type=photo
-            xhr.open('GET', 'https://pixabay.com/api/?key=5553692-b6d8cb2b30635222df6f2566b&q='+r+'&min_width=700&lang=en+ru', true);
-            xhr.send();
-            xhr.onreadystatechange = function() {
-              if (this.readyState != 4) return;
-              if (this.status != 200) {
-                // обработать ошибку
-                alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
-                return;
+            function sender(r) {
+              var xhr = new XMLHttpRequest();                                                        //&image_type=photo
+              xhr.open('GET', 'https://pixabay.com/api/?key=5553692-b6d8cb2b30635222df6f2566b&q='+r+'&min_width=700&lang=en+ru', true);
+              xhr.send();
+              xhr.onreadystatechange = function() {
+                if (this.readyState != 4) return;
+                if (this.status != 200) {
+                  // обработать ошибку
+                  alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
+                  return;
+                }
+                var baseImg = JSON.parse(this.responseText);
+                _.forEach(baseImg.hits, function(value, key) {
+                  model.data.addItem([value.webformatURL, value.tags]);
+                });
+                that.load = 1;
+
+                var timerViewRenderImg = setInterval(view.renderImg(model), 200);
+                if (this.readyState === 4) {
+                  setTimeout(function() {
+                    clearInterval(timerViewRenderImg);
+                  }, 2000);
+                }
+                // view.renderImg(model);
               }
-              var baseImg = JSON.parse(this.responseText);
-              _.forEach(baseImg.hits, function(value, key) {
-                model.data.addItem([value.webformatURL, value.tags]);
-              });
-              that.load = 1;
-
-              var timerViewRenderImg = setInterval(view.renderImg(model), 200);
-              if (this.readyState === 4) {
-                setTimeout(function() {
-                  clearInterval(timerViewRenderImg);
-                }, 2000);
-              }
-              // view.renderImg(model);
             }
-          }
 
-          function sendRequest(e) {
-            // e.preventdefault();
-            var textForm = view.element.inputIdeas.value;
-            //API request:
-            model.data.deleteBase();
-            sender(textForm);
+            function sendRequest(e) {
+              // e.preventdefault();
+              var textForm = view.element.inputIdeas.value;
+              //API request:
+              model.data.deleteBase();
+              sender(textForm);
               // end API
               return e;
               //end click
@@ -72,6 +73,11 @@ try {
             // })
 
             that.sendRequest=sendRequest();
+          } catch (e) {
+
+          } finally {
+
+          }
 
             return this;
           },
